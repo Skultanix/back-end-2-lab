9b06a73409a7478c4cb87258b49667067302770b
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-01 14:24:42 -0700

    Endpoints set up in index file, listener set up.

--- server/index.js
+++ server/index.js
@@ -7,4 +7,17 @@ app.use(express.json())
 app.use(cors())
 
 //variable requiring controller file.
-const {getHouses, createHouse, updateHouse, deleteHouse} = require('./controller')
+const {
+    getHouses,
+     createHouse,
+      updateHouse,
+       deleteHouse
+    } = require('./controller')
+
+//endpoint setup
+app.get(`/api/houses`, getHouses)
+app.delete(`/api/house/:id`, deleteHouse)
+app.post(`/api/houses`, createHouse)
+app.put(`/api/houses/:id`, updateHouse)
+
+app.listen(4004, () => console.log(`Port 4004 locked and loaded.`))
