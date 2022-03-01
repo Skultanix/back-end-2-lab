3de1e10269afef4b1c23bf3c12f7e6d031e2b36f
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-01 14:44:46 -0700

    Functionality written.

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,41 @@
-module.exports {
-    function getHouses() {
+const e = require("express")
+const houseDB = require(`./db.json`)
+let houseID = 4
 
+module.exports = {
+getAllHouses: (req, res) => res.status(200).send(houseDB),
+deleteHouse: (req, res) => {
+    let index = houseDB.findIndex(ele => ele.id === +req.params.id)
+    houseDB.splice(index, 1)
+    return res.status(200).send(houseDB)
+},
+createHouse: (req, res) => {
+    let {address, price, imageURL} = req.body
+    let newHouse = {
+        id: houseDB,
+        address,
+        price,
+        imageURL
     }
-    function createHouse() {
-
+    houseDB.push(newHouse)
+    return res.status(200).send(houseDB)
+    houseID++
+},
+updateHouse: (req, res) => {
+    let {id} = req.params
+    let {type} = req.body
+    let index = houseDB.findIndex(ele => ele.id === +id)
+    if(houseDB[index].price <= 10000 && type === `minus`) {
+        houseDB[index].price = 0
+        res.status(200).send(houseDB)
+    } else if(type === `plus`) {
+        houseDB[index].price += 10000
+        res.status(200).send(houseDB)
+    } else if(type === `minus`) {
+        houseDB[index].price -= 10000
+        res.status(200).send(houseDB)
+    } else {
+        res.status(400)
     }
-    function updateHouse() {
-
-    }
-    function deleteHouse() {
-        
-    }
+}
 }
